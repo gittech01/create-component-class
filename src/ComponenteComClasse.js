9a1f21efab8091518344b492d94de76534f399f1
Edson Consegue <edsonconsegue990@gmail.com>
2022-12-04 15:13:26 -0300

    Update function with events

--- src/ComponenteComClasse.js
+++ src/ComponenteComClasse.js
@@ -1,32 +1,67 @@
 import React from 'react';
 
 
-// Criar componente com classe
-class Componente extends React.Component {
+// Criar componente com classe precisamos antes importar o modulo acima
+class ComponenteComClasse extends React.Component {
 
-  // arrow function
-  // arrow function sem paramentro
-  botao1 = () => {
-    alert("Botão 1 Clicado!!!");
+  // ARROW FUNCTION
+  // sem paramentro
+  botaoSemParam1 = () => {
+    alert("Funçao sem parametro 1, Acionada!!!");
+
   }
 
-  // arrow function com parametro
-  botao2 = (curso) => {
-    alert("Botão 2, apresenta " + curso);
+  botaoSemParam2 = () => {
+    alert("Funçao sem parametro 2, Acionada!!!");
+  }
+
+  // com parametro
+  botaoComParam1 = (param1) => {
+    alert("Funçao com parametro 1, " + param1);
+  }
+
+  botaoComParam2 = (param2) => {
+    alert("Funçao Acionada parametro 2, " + param2);
+  }
+
+  //arrow function teclado (o obj pode ser um e, event..., na verdade o nome que quiseres)
+  teclado = (obj) => {
+    console.log(obj.target.value)
   }
 
   // para uma classe sempre deve existir o metedo render
   render() {
     return (
       <div>
-        <p>Mostramos a baixo como chamar as arrow function dentro de uma Componente com Classe </p>
-        <button onClick={() => this.botao1()}>Botão 1</button>
+        <h1>Treinando como chamar funcção com e sem parametros </h1>
+
+        {/* 2 FORMAS DE CHAMAR FUNÇÕES SEM PARAMETROS */}
+        {/* Tipo 1: */}
+        <button onClick={() => this.botaoSemParam1()}>Botão Sem parameto 1</button>
+
+        {/* Tipo 2: */}
+        <button onClick={this.botaoSemParam2}>Botão Sem parameto 2</button>
         <br></br>
-        <button onClick={() => this.botao2("ReactJS")}>Botão 2</button>
+
+        <hr />
+
+        {/*  2 FORMAS DE CHAMAR FUNÇÃO COM PARAMETROS */}
+        {/* Tipo 1: usando arrow function*/}
+        <button onClick={() => this.botaoComParam1("Acionado!!!")}>Botão Com parameto 1</button>
+
+        {/* Tipo 2: usando o bind*/}
+        <button onClick={this.botaoComParam2.bind(this, "Acionado!!!")}>Botão Com parameto 2</button>
+        <hr />
+        <br />
+
       </div>
     )
   }
 
 }
 
-export default Componente;
+export default ComponenteComClasse;
+
+
+// mover linha para baixo: alt + up or down
+
